519168d35ae189f8ea732ecbbdfb65f88fa96e73
tobak <tobak@protonmail.com>
2024-02-19 11:41:45 +0100

    introduce UNIT_TESTS

--- src/test/model/container/test_container.cpp
+++ src/test/model/container/test_container.cpp
@@ -1,1 +1,44 @@
 #include <gtest/gtest.h>
+#include "../../../main/model/container/Container.h"
+
+TEST(ContainerTest, TestRandomness) {
+    auto *container1 = new Container<int>();
+    auto *container2 = new Container<int>();
+
+//    auto *container1 = new Container<Parameters>();
+
+    int four = 4;
+    int two = 2;
+    int one = 1;
+
+    EXPECT_TRUE(container1->isEmpty());
+    EXPECT_TRUE(container2->isEmpty());
+
+    container1->add(four);
+    container1->add(two);
+    container1->add(one);
+    container1->remove(1); //by pointer instead of index if possible
+    container1->add(two);
+
+    EXPECT_EQ(container1->size(), 3);
+    EXPECT_FALSE(container1->isEmpty());
+
+
+    std::cout << container1->accessElement(0) << std::endl;
+
+    container2->add(four);
+    container2->add(four);
+    container2->add(four);
+    container2->add(four);
+    container2->add(four);
+
+    EXPECT_EQ(container2->size(), 5);
+    EXPECT_FALSE(container2->isEmpty());
+
+    container1->merge(*container2);
+    EXPECT_EQ(container1->size(), 8);
+
+    delete container1;
+    delete container2;
+
+}
