a4092f31cd2f10f6d9e6bd8b7a4a2dd2a5ea77bf
IreneGR92 <igaru.13@gmail.com>
2024-03-16 10:16:36 -0400

    insomnia config

--- run_insomnia.sh
+++ run_insomnia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 srun -A e3b --pty -t 0-02:00 -X /bin/bash
-cd ../../
+
 rm -rf build
 mkdir build
-
 cd build
 cmake ..
 make
 cd ..
-cd deployment/insomnia
+exit
+
 sbatch insomnia.sh
