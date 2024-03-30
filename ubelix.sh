6e81881c87adcd55b7f9944a27ed2ceda7bf6055
IreneGR92 <igaru.13@gmail.com>
2024-03-30 16:45:11 -0400

    ubelix yml

--- ubelix.sh
+++ ubelix.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # You must specify a valid email address!
-#SBATCH --mail-user=ig17c521@campus.unibe.ch
+#SBATCH --mail-user=ig17c521@campus.unibe.ch, ig2500@columbia.edu
 
 # Mail on NONE, BEGIN, END, FAIL, REQUEUE, ALL
 #SBATCH --mail-type=end,fail
 
 # Job name
-#SBATCH --job-name="ReproductiveSkew"
+#SBATCH --job-name="ReprSkew"
 
 # Runtime and memory
 #SBATCH --time=96:00:00
@@ -29,37 +29,75 @@
 
 # For array jobs
 # Indicate how many input files you have
-#SBATCH --array=0-11
+#SBATCH --array=0-60
 
 #### Your shell commands below this line ####
 
 declare -a arr=(
 
-"high_adult_mortality.yml"
-"high_adult_mortality_NoRelat.yml"
-"m1_rate0.yml"
-#"m1_rate0_h0.yml"
-"m1_rate03.yml"
-#"m1_rate03_h0.yml"
-"m04_rate0.yml"
-#"m04_rate0_h0.yml"
-"m025_rate03.yml"
-#"m025_rate03_h0.yml"
-"m025_rate05.yml"
-#"m025_rate05_h0.yml"
+"Immigration0_m1_rate0_default.yml"
+"Immigration0_m1_rate0_NoRelatedness.yml"
+"Immigration0_m1_rate03_default.yml"
+"Immigration0_m1_rate03_NoRelatedness.yml"
+"Immigration0_m04_rate0_default.yml"
+"Immigration0_m04_rate0_NoRelatedness.yml"
+"Immigration0_m025_rate03_default.yml"
+"Immigration0_m025_rate03_NoRelatedness.yml"
+"Immigration0_m025_rate05_default.yml"
+"Immigration0_m025_rate05_NoRelatedness.yml"
+"Immigration1_m1_rate0_default.yml"
+"Immigration1_m1_rate0_NoRelatedness.yml"
+"Immigration1_m1_rate03_default.yml"
+"Immigration1_m1_rate03_NoRelatedness.yml"
+"Immigration1_m04_rate0_default.yml"
+"Immigration1_m04_rate0_NoRelatedness.yml"
+"Immigration1_m025_rate03_default.yml"
+"Immigration1_m025_rate03_NoRelatedness.yml"
+"Immigration1_m025_rate05_default.yml"
+"Immigration1_m025_rate05_NoRelatedness.yml"
+"Immigration05_m1_rate0_default.yml"
+"Immigration05_m1_rate0_NoRelatedness.yml"
+"Immigration05_m1_rate03_default.yml"
+"Immigration05_m1_rate03_NoRelatedness.yml"
+"Immigration05_m04_rate0_default.yml"
+"Immigration05_m04_rate0_NoRelatedness.yml"
+"Immigration05_m025_rate03_default.yml"
+"Immigration05_m025_rate03_NoRelatedness.yml"
+"Immigration05_m025_rate05_default.yml"
+"Immigration05_m025_rate05_NoRelatedness.yml"
 
-"m1_rate0_NoRelatedness.yml"
-#"m1_rate0_h0_NoRelatedness.yml"
-"m1_rate03_NoRelatedness.yml"
-#"m1_rate03_h0_NoRelatedness.yml"
-"m04_rate0_NoRelatedness.yml"
-#"m04_rate0_h0_NoRelatedness.yml"
-"m025_rate03_NoRelatedness.yml"
-#"m025_rate03_h0_NoRelatedness.yml"
-"m025_rate05_NoRelatedness.yml"
-#"m025_rate05_h0_NoRelatedness.yml"
+"RS0_m1_rate0_default.yml"
+"RS0_m1_rate0_NoRelatedness.yml"
+"RS0_m1_rate03_default.yml"
+"RS0_m1_rate03_NoRelatedness.yml"
+"RS0_m04_rate0_default.yml"
+"RS0_m04_rate0_NoRelatedness.yml"
+"RS0_m025_rate03_default.yml"
+"RS0_m025_rate03_NoRelatedness.yml"
+"RS0_m025_rate05_default.yml"
+"RS0_m025_rate05_NoRelatedness.yml"
+"RS1_m1_rate0_default.yml"
+"RS1_m1_rate0_NoRelatedness.yml"
+"RS1_m1_rate03_default.yml"
+"RS1_m1_rate03_NoRelatedness.yml"
+"RS1_m04_rate0_default.yml"
+"RS1_m04_rate0_NoRelatedness.yml"
+"RS1_m025_rate03_default.yml"
+"RS1_m025_rate03_NoRelatedness.yml"
+"RS1_m025_rate05_default.yml"
+"RS1_m025_rate05_NoRelatedness.yml"
+"RS05_m1_rate0_default.yml"
+"RS05_m1_rate0_NoRelatedness.yml"
+"RS05_m1_rate03_default.yml"
+"RS05_m1_rate03_NoRelatedness.yml"
+"RS05_m04_rate0_default.yml"
+"RS05_m04_rate0_NoRelatedness.yml"
+"RS05_m025_rate03_default.yml"
+"RS05_m025_rate03_NoRelatedness.yml"
+"RS05_m025_rate05_default.yml"
+"RS05_m025_rate05_NoRelatedness.yml"
 
 
 		)
 
-srun ./build/App "parameters/${arr[$SLURM_ARRAY_TASK_ID]}"
+srun ./build/App "paramete"RS/${arr[$SLURM_ARRAY_TASK_ID]}"
