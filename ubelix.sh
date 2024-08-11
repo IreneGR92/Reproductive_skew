2104d6c316edf3dce95c7a2bca19f4ca00b60c1b
IreneGR92 <igaru.13@gmail.com>
2024-08-11 21:43:22 +0200

    ubelix

--- ubelix.sh
+++ ubelix.sh
@@ -29,37 +29,20 @@
 
 # For array jobs
 # Indicate how many input files you have
-#SBATCH --array=0-23
+#SBATCH --array=0-7
 
 #### Your shell commands below this line ####
 
 declare -a arr=(
 
-"Immigration0_m04_rate03_default.yml"
-"Immigration0_m04_rate03_NoRelatedness.yml"
-"Immigration0_m025_rate0_default.yml"
-"Immigration0_m025_rate0_NoRelatedness.yml"
-"Immigration1_m04_rate03_default.yml"
-"Immigration1_m04_rate03_NoRelatedness.yml"
-"Immigration1_m025_rate0_default.yml"
-"Immigration1_m025_rate0_NoRelatedness.yml"
-"Immigration05_m04_rate03_default.yml"
-"Immigration05_m04_rate03_NoRelatedness.yml"
-"Immigration05_m025_rate0_default.yml"
-"Immigration05_m025_rate0_NoRelatedness.yml"
-
-"RS0_m04_rate03_default.yml"
-"RS0_m025_rate0_NoRelatedness.yml"
-"RS0_m025_rate0_default.yml"
-"RS0_m04_rate03_NoRelatedness.yml"
-"RS1_m04_rate03_default.yml"
-"RS1_m04_rate03_NoRelatedness.yml"
-"RS1_m025_rate0_NoRelatedness.yml"
-"RS1_m025_rate0_default.yml"
-"RS05_m04_rate03_default.yml"
-"RS05_m04_rate03_NoRelatedness.yml"
-"RS05_m025_rate0_default.yml"
-"RS05_m025_rate0_NoRelatedness.yml"
+"NoBH_m025_rate0_NoRelatedness.yml"
+"NoBH_m025_rate0_h0_default.yml"
+"NoBH_m04_rate03_NoRelatedness.yml"
+"NoBH_m025_rate0_h0_NoRelatedness.yml"
+"NoBH_m04_rate03_default.yml"
+"NoBH_m025_rate0_default.yml"
+"NoBH_m04_rate03_h0_default.yml"
+"NoBH_m04_rate03_h0_NoRelatedness.yml"
 
 
 		)
