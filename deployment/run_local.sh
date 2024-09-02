59d965bd4ef9a95e1c75c2cbb37817487bdab53e
IreneGR92 <igaru.13@gmail.com>
2024-09-02 22:56:05 +0200

    ubelix

--- deployment/run_local.sh
+++ deployment/run_local.sh
@@ -3,34 +3,34 @@
 
 declare -a arr=(
 
-NoBH_m1_rate0_default.yml
-NoBH_m1_rate0_NoRelatedness.yml
-NoBH_m1_rate0_Xrs0_default.yml
-NoBH_m1_rate0_Xrs0_NoRelatedness.yml
-NoBH_m1_rate03_default.yml
-NoBH_m1_rate03_NoRelatedness.yml
-NoBH_m1_rate03_Xrs0_default.yml
-NoBH_m1_rate03_Xrs0_NoRelatedness.yml
-NoBH_m04_rate0_default.yml
-NoBH_m04_rate0_NoRelatedness.yml
-NoBH_m04_rate0_Xrs0_default.yml
-NoBH_m04_rate0_Xrs0_NoRelatedness.yml
-NoBH_m04_rate03_default.yml
-NoBH_m04_rate03_NoRelatedness.yml
-NoBH_m04_rate03_Xrs0_default.yml
-NoBH_m04_rate03_Xrs0_NoRelatedness.yml
-NoBH_m025_rate0_default.yml
-NoBH_m025_rate0_NoRelatedness.yml
-NoBH_m025_rate0_Xrs0_default.yml
-NoBH_m025_rate0_Xrs0_NoRelatedness.yml
-NoBH_m025_rate03_default.yml
-NoBH_m025_rate03_NoRelatedness.yml
-NoBH_m025_rate03_Xrs0_default.yml
-NoBH_m025_rate03_Xrs0_NoRelatedness.yml
-NoBH_m025_rate05_default.yml
-NoBH_m025_rate05_NoRelatedness.yml
-NoBH_m025_rate05_Xrs0_default.yml
-NoBH_m025_rate05_Xrs0_NoRelatedness.yml
+"NoBH_m1_rate0_default.yml"
+"NoBH_m1_rate0_NoRelatedness.yml"
+"NoBH_m1_rate0_Xrs0_default.yml"
+"NoBH_m1_rate0_Xrs0_NoRelatedness.yml"
+"NoBH_m1_rate03_default.yml"
+"NoBH_m1_rate03_NoRelatedness.yml"
+"NoBH_m1_rate03_Xrs0_default.yml"
+"NoBH_m1_rate03_Xrs0_NoRelatedness.yml"
+"NoBH_m04_rate0_default.yml"
+"NoBH_m04_rate0_NoRelatedness.yml"
+"NoBH_m04_rate0_Xrs0_default.yml"
+"NoBH_m04_rate0_Xrs0_NoRelatedness.yml"
+"NoBH_m04_rate03_default.yml"
+"NoBH_m04_rate03_NoRelatedness.yml"
+"NoBH_m04_rate03_Xrs0_default.yml"
+"NoBH_m04_rate03_Xrs0_NoRelatedness.yml"
+"NoBH_m025_rate0_default.yml"
+"NoBH_m025_rate0_NoRelatedness.yml"
+"NoBH_m025_rate0_Xrs0_default.yml"
+"NoBH_m025_rate0_Xrs0_NoRelatedness.yml"
+"NoBH_m025_rate03_default.yml"
+"NoBH_m025_rate03_NoRelatedness.yml"
+"NoBH_m025_rate03_Xrs0_default.yml"
+"NoBH_m025_rate03_Xrs0_NoRelatedness.yml"
+"NoBH_m025_rate05_default.yml"
+"NoBH_m025_rate05_NoRelatedness.yml"
+"NoBH_m025_rate05_Xrs0_default.yml"
+"NoBH_m025_rate05_Xrs0_NoRelatedness.yml"
 
 )
 
