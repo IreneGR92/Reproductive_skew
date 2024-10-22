0e52d77604a63326442d970821295eadab7c6de8
tobak <tobak@protonmail.com>
2024-10-22 13:39:12 +0200

    minor improvements

--- src/main/util/FilePrinter.h
+++ src/main/util/FilePrinter.h
@@ -3,20 +3,20 @@
 
 
 #include <string>
-#include <memory>
 #include "Parameters.h"
 #include "ResultCache.h"
 
 class FilePrinter {
-private:
+    const int PRECISION = 4;
+
     std::shared_ptr<Parameters> parameters;
 
     void printHeader(std::ofstream &writer);
 
     static void writeResults(std::ofstream &writer, const std::vector<std::string> &cache);
 
-    std::ofstream *mainWriter; ///< A pointer to the main output stream writer.
-    std::ofstream *lastGenerationWriter; ///< A pointer to the last generation output stream writer.
+    std::unique_ptr<std::ofstream> mainWriter; ///< A pointer to the main output stream writer.
+    std::unique_ptr<std::ofstream> lastGenerationWriter; ///< A pointer to the last generation output stream writer.
 
 public:
     explicit FilePrinter(std::shared_ptr<Parameters> parameters);
